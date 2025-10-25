620b9f415ab046f522deedb36d639e3b430a4a23
ra-kesh <iamrakeshkumar@pm.me>
2025-10-25 17:25:11 +0530

    updated next js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  reactCompiler: true,
 };
 
 export default withPayload(nextConfig);
